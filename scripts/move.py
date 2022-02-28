5c60cbf3351b5df31c7b27af6d0d4ff9bf35b9cf
zhuzy <zhuzy_2013@163.com>
2022-02-28 20:58:44 +0800

    move and record code done

--- scripts/move.py
+++ scripts/move.py
@@ -7,32 +7,39 @@ from actionlib_msgs.msg import GoalStatus
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from geometry_msgs.msg import Pose,Point,Quaternion,PoseWithCovarianceStamped
 from nav_msgs.msg import Odometry
+from std_srvs.srv import Trigger,TriggerRequest
 import time
+import thread
 class random_move:
 
     pose_list = []
     temp_pose_list = []
     success_move = 0
+    total_move = 0
     
     
     def __init__(self):
         rospy.init_node('movebase_client_py')
         self.initial_pose_pub = rospy.Publisher('/initialpose',PoseWithCovarianceStamped,queue_size=10)
         rospy.Subscriber("odom", Odometry, self.odom_callback)
+        rospy.wait_for_service('/reinitial_record')
+        rospy.wait_for_service('/success_stop_record')
+        self.reinitial_record = rospy.ServiceProxy('/reinitial_record',Trigger)
+        self.success_stop_record = rospy.ServiceProxy('/success_stop_record',Trigger)
         self.client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
         self.client.wait_for_server()
         self.get_pose()
         self.main_loop()
 
     def odom_callback(self, data):#fps:30hz
-        rospy.loginfo("odom pose: x:%0.6f , y:%0.6f",data.pose.pose.position.x,data.pose.pose.position.y)
-        self.real_pose = data.pose
+        # rospy.loginfo("odom pose: x:%0.6f , y:%0.6f",data.pose.pose.position.x,data.pose.pose.position.y)
+        self.real_pose = data.pose.pose
 
     def pub_initial_pose(self,pose):
         initial_pose = PoseWithCovarianceStamped()
         initial_pose.header.frame_id = "map"
         initial_pose.header.stamp = rospy.Time.now()
-        initial_pose.pose = pose
+        initial_pose.pose.pose = pose
         self.initial_pose_pub.publish(initial_pose)
 
     def movebase_client(self):
@@ -41,8 +48,8 @@ class random_move:
         goal = MoveBaseGoal()
         goal.target_pose.header.frame_id = "map"
         goal.target_pose.header.stamp = rospy.Time.now()
-        goal.target_pose.pose = self.temp_pose_list[random.randint(0,len(self.temp_pose_list)-1)]
-        # goal.target_pose.pose = self.temp_pose_list.pop(random.randint(0,len(self.temp_pose_list)-1))
+        # goal.target_pose.pose = self.temp_pose_list[random.randint(0,len(self.temp_pose_list)-1)]
+        goal.target_pose.pose = self.temp_pose_list.pop(random.randint(0,len(self.temp_pose_list)-1))
         # goal.target_pose.pose.position.x = 0.5
         # goal.target_pose.pose.position.y = 0.5
         # goal.target_pose.pose.orientation.z = 0.702
@@ -71,23 +78,42 @@ class random_move:
 
         self.temp_pose_list = self.pose_list
 
-    def main_loop(self):
-        #start
-        self.pub_initial_pose(self.real_pose)
-        start_time = time.time()
-        #todo: start to record plot data
+    def kidnap(self,start_time):
+        while (time.time() - self.start_time < start_time):
+            time.sleep(1)
+        #loop end
+        rand_pose = self.temp_pose_list[random.randint(0,len(self.temp_pose_list)-1)]
+        self.pub_initial_pose(rand_pose)
+        rospy.loginfo("kidnapped pose: x:%0.6f , y:%0.6f",rand_pose.position.x,rand_pose.position.y)
 
-        while (self.success_move <= 2)and(not rospy.is_shutdown()):
-            result,state = self.movebase_client()
-            if state == GoalStatus.SUCCEEDED:
-                rospy.loginfo("the result of sever is:success")
-                self.success_move += 1
-            else:
-                rospy.loginfo("move is not success, the state is "+ str(state))
+
+
+    def main_loop(self):
+        while (self.total_move <= 100)and(not rospy.is_shutdown()):
+            while (len(self.temp_pose_list) >= 2)and(not rospy.is_shutdown()):
+                #start
+                self.pub_initial_pose(self.real_pose)
+                self.start_time = time.time()
+                thread.start_new_thread(self.kidnap,(10,))
+                self.reinitial_record(TriggerRequest())
+                #todo: start to record plot data
+                result,state = self.movebase_client()
+                self.total_move += 1
+                if state == GoalStatus.SUCCEEDED:
+                    rospy.loginfo("the result of sever is:success| total move = %d",self.total_move)
+                    self.success_move += 1
+                    self.success_stop_record(TriggerRequest())
+                else:
+                    rospy.loginfo("move is not success, the state is "+ str(state) +"| total move = %d",self.total_move)
+
+            self.temp_pose_list = self.pose_list
+
+        rospy.loginfo("|success move/total move: %d / %d|",self.success_move,self.total_move)
+
+            
 
 if __name__ == '__main__':
     try:
-        
         mv = random_move()
         # mv.add_pose(Pose(Point(-1,-2,0),Quaternion(0,0,0,1)))
         # mv.get_pose()
--- scripts/mse.py
+++ scripts/mse.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 from rosdep2 import RosdepLookup
 import rospy
+import rospkg
 from matplotlib import pyplot as plt
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from std_srvs.srv import Trigger, TriggerResponse
 import time
 import math
+import csv
 
-class Visualiser():
+class Recorder():
     #although variates below is belong to the whole class , we only use them once anyway
     odom_time, cb_time = 0, 0
     x_odom = None
@@ -21,6 +24,7 @@ class Visualiser():
         self.fig, self.ax = plt.subplots()
         self.ln, = plt.plot([], [], 'ro')
         self.init_time = time.time()
+        self.rospack = rospkg.RosPack()
 
     
     def odom_callback(self, data):#fps:30hz
@@ -33,10 +37,32 @@ class Visualiser():
     def amcl_pose_callback(self,data):#fps:1hz
         rospy.loginfo("amcl pose: x:%0.6f , y:%0.6f",data.pose.pose.position.x,data.pose.pose.position.y)
         self.x_pose = data.pose.pose.position.x
-        self.y_pose = data.pose.pose.position.y  
-        rmse = math.sqrt(math.pow((self.x_odom - self.x_pose),2) + math.pow((self.y_odom - self.y_pose),2))
-        self.rmses.append(rmse)
-        self.rmse_times.append(time.time() - self.init_time)
+        self.y_pose = data.pose.pose.position.y
+        if self.x_odom != None:  
+            rmse = math.sqrt(math.pow((self.x_odom - self.x_pose),2) + math.pow((self.y_odom - self.y_pose),2))
+            self.rmses.append(rmse)
+            self.rmse_times.append(time.time() - self.init_time)
+
+    def write2csv(self,datalist,file = "no_algo.csv"):
+        with open(file,'a') as f:
+            cw  = csv.writer(f)
+            cw.writerow(datalist) 
+    
+    def reinitial_callback(self,req):
+
+        # rospy.signal_shutdown("servive signal stop record")#initial shutdown process
+        del self.rmses[:]
+        del self.rmse_times[:]
+        self.init_time = time.time()
+        rospy.loginfo("record reinitial!")
+
+        return TriggerResponse(1, "record reinitial!")
+
+    def success_shutdown_callback(self,req):
+
+        self.write2csv(self.rmses,file=self.rospack.get_path("sim_localization")+"/no_algo.csv")
+        rospy.loginfo("stop record!")
+        return TriggerResponse(1, "stop record!")
         
 
 
@@ -44,15 +70,19 @@ class Visualiser():
 
 
 if __name__ == "__main__":
-    rospy.init_node("visualiser")
-    vis = Visualiser()
+    rospy.init_node("Recorder")
+    vis = Recorder()
 
     # gazebo's odom, without nosie
     rospy.Subscriber("odom", Odometry, vis.odom_callback)
     rospy.Subscriber("amcl_pose",PoseWithCovarianceStamped,vis.amcl_pose_callback)
+    rospy.Service('/reinitial_record',Trigger,vis.reinitial_callback)
+    rospy.Service('/success_stop_record',Trigger,vis.success_shutdown_callback)
     rospy.spin()
     if rospy.is_shutdown():
-        plt.plot(vis.rmse_times, vis.rmses, color="b", label="/odom")
-        plt.show()
+        pass
+        # vis.write2csv(vis.rmses)
+        # plt.plot(vis.rmse_times, vis.rmses, color="b", label="/odom")
+        # plt.show()
 
 
